98d50cbea4792d4975d77abfdffb98f9d4d8ff48
domonikk <domonique.eccleston@gmail.com>
2021-08-20 00:59:42 -0500

    update

--- covid-tracker/src/components/DataBoxes.vue
+++ covid-tracker/src/components/DataBoxes.vue
@@ -15,13 +15,13 @@
 
         <!--Box 2-->
         <div class="shadow-md bg-blue-200 p-10 text-center rounded">
-            <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3> 
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">Deaths</h3> 
             <div class="text-2xl mb-4">
-                <span class="font-bold">New Deaths :</span> 
+                <span class="font-bold">New  :</span> 
                 {{numberWithCommas(stats.NewDeaths)}} 
             </div>
             <div class="text-2xl mb-4">
-                <span class="font-bold">Total Deaths:</span> 
+                <span class="font-bold">Total :</span> 
                 {{numberWithCommas(stats.TotalDeaths)}} 
             </div>
         </div>
